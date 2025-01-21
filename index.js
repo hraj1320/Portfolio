1982ce5d72721bd6c192ad0406b4cb85e1eab735
hraj1320 <hraj000123@gmail.com>
2025-01-21 16:11:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+ function home (){
+    document.getElementById("home").scrollIntoView({behavior:"smooth"})
+ }
+
+ function about (){
+    document.getElementById("ab").scrollIntoView({behavior:"smooth"})
+ }
+ function skill (){
+    document.getElementById("skill").scrollIntoView({behavior:"smooth"})
+ }
+ function cont (){
+    document.getElementById("cont").scrollIntoView({behavior:"smooth"})
+ }
+ let a= document.querySelector("#home")
